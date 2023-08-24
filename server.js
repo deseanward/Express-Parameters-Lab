ef3886cfdbea578cd1227dfac9524b15ce696639
De Sean Ward <d.ward.chi@gmail.com>
2023-08-24 12:21:33 -0500

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Import Express
-const express = require('express');
+const express = require("express");
 
 // Create an instance of express
 const app = express();
@@ -7,19 +7,51 @@ const app = express();
 // Create a variable for the port
 const port = 3000;
 
+// Responses for 8 Ball quotes
+const quotes = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
 // Create routes
-app.get('/greeting/:name', (req, res) => {
-    const greeting =
-    res.send(`<h1>Hello, ${req.params.name}!</h1>`)
-})
+// Greeting Route
+app.get("/greeting/:name", (req, res) => {
+  res.send(`<h1>Hello, ${req.params.name}!</h1>`);
+});
 
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-    console.log(req.params)
-    res.send(`Tip is ${req.params.tipPercentage}`)
-})
+// Tip Calculator Route
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  console.log(req.params);
+  res.send(`Tip is ${req.params.tipPercentage}`);
+});
 
+// Magic 8 Ball Route
+app.get("/magic/:question", (req, res) => {
+  console.log(req.params);
+
+  const idx = Math.floor(Math.random() * quotes.length);
+  res.send(`<h1>Will you become a millionaire...? ${quotes[idx]}</h1>`)
+});
 
 // Listen to the port
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
